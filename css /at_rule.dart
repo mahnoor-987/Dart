a99221087d66c0ad6e289174e8678a4784820909
Mahnoor Fatima <aminadreamy568@gmail.com>
2025-08-29 10:31:16 +0500

    Update at_rule.dart

--- css /at_rule.dart
+++ css /at_rule.dart
@@ -1,7 +1,3 @@
-// Copyright 2016 Google Inc. Use of this source code is governed by an
-// MIT-style license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 import 'node.dart';
 import 'value.dart';
 
