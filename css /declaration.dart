bd5a7474774e8d04f8f38737153ba1db47bce305
Mahnoor Fatima <aminadreamy568@gmail.com>
2025-08-30 21:20:40 +0500

    Update declaration.dart

--- css /declaration.dart
+++ css /declaration.dart
@@ -1,6 +1,3 @@
-// Copyright 2016 Google Inc. Use of this source code is governed by an
-// MIT-style license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
 
 import 'package:meta/meta.dart';
 import 'package:source_span/source_span.dart';
